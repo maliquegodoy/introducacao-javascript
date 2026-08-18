dd0ac25a53e7cd408fd800e812bf95ecd766a811
caduzol <dudagodoysoares@gmail.com>
2026-08-18 16:46:06 -0300

    upload

---
+++ introducao-javascript/05-futebol/aula5.js
@@ -0,0 +1,9 @@
+//Atividade 5
+(E) . (OU);
+const pontos = 40;
+const saldoDeGols = 5;
+const venceuUltimoJogo = true;
+const classificados = pontos >=35 && (saldoDeGols > 0 ||, venceuUltimoJogo)
+
+console.log(classificados)
+document.getElementById("resultado")
